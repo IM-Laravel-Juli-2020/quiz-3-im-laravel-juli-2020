1efac4217de2a9008487c213430e15674ef81ac6
nabilavlnk <valinkanabila@gmail.com>
2020-08-09 22:24:36 +0700

    kuiz 3

---
+++ database/migrations/2020_08_09_135523_create_staff_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStaffTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('staff', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nama');
+            $table->string('jabatan');
+            $table->unsignedBigInteger('manajer_id');
+            $table->foreign('manajer_id')->references('id')->on('manajer');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('staff');
+    }
+}
