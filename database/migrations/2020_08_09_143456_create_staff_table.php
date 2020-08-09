f6985265427c3983b9026c0b1a0bedf579624ee7
Sofy Amartya <sofy_amartya@yahoo.com>
2020-08-09 22:19:50 +0700

    quiz week 3

---
+++ database/migrations/2020_08_09_143456_create_staff_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStaffTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('staff', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->unsignedBigInteger('manajer_id');
+            $table->foreign('manajer_id')->references('id')->on('manajer');
+
+            $table->unsignedBigInteger('proyek_id');
+            $table->foreign('proyek_id')->references('id')->on('proyek');
+            //$table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('staff');
+    }
+}
