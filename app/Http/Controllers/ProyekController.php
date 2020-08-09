717afa7443ddc11241817029a548e7bada6bac97
nurrahmaaz <ammaazharii@gmail.com>
2020-08-09 23:22:23 +0800

    tugas setengah selesai

---
+++ app/Http/Controllers/ProyekController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProyekController extends Controller
+{
+    //
+    public function gambar(){
+    	<img src=" {{('img/erd.png')}} ">
+
+    return view('/');
+    }
+}
