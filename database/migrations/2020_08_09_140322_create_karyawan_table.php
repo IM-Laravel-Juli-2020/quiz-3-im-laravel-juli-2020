370ae4593081c5dd047ab25f3c055cce8597eba0
HarisV <ezlife9909@gmail.com>
2020-08-09 22:28:18 +0700

    QUIZ SELESAI

--- database/migrations/2020_08_09_140322_create_karyawan_table.php
+++ database/migrations/2020_08_09_140322_create_karyawan_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePasswordResetsTable extends Migration
+class CreateKaryawanTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,13 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('karyawan', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('proyek_id')->nullable();
+            $table->string('nama');
+            $table->enum('jabatan', ['staff', 'manager']);
+            
+
         });
     }
 
@@ -27,6 +30,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('karyawan');
     }
 }
