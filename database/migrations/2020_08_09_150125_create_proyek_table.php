5c0ba1f485318111e0695b4b8e944af7342f18f0
dewindeadew <justadewin@yahoo.com>
2020-08-09 22:31:30 +0700

    tugas kuis belum selesai huhu

---
+++ database/migrations/2020_08_09_150125_create_proyek_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProyekTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('proyek', function (Blueprint $table) {
+            $table->bigIncrements('id_proyek');
+            $table->string('nama_proyek');
+            $table->longText('deskripsi_proyek');
+            $table->date('tgl_mulai');
+            $table->date('tgl_deadline');
+            $table->boolean('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('proyek');
+    }
+}
