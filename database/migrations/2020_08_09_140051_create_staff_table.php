31238e41b251e2c6b75ce54f48cc9f43501d1cfb
Muhammad Abdurasyid <mhdabdurasyid@users.noreply.github.com>
2020-08-09 22:32:26 +0700

    add jawaban quis 3

---
+++ database/migrations/2020_08_09_140051_create_staff_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStaffTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('staff', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('id_karyawan')->nullable();
+            $table->foreign('id_karyawan')->references('id')->on('karyawan');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('staff');
+    }
+}
